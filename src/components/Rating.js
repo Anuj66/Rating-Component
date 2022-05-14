c4ffc8455e9ee87bb3f2236e0b3eb0468f39d3de
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2022-05-14 19:04:03 +0530

    Made changes in Rating.css

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,39 +1,44 @@
 import "./Rating.css";
 import iconStar from "../images/icon-star.svg";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import RatingCard from "./RatingCard";
 
 const Rating = () => {
-  const [rating, setRating] = useState(0);
+    const [rating, setRating] = useState(0);
 
-  useEffect(() => {
-    console.log(rating);
-  }, [rating]);
+    useEffect(() => {
+        console.log(rating);
+    }, [rating]);
 
-  return (
-    <>
-      <div className="image-star">
-        <img src={iconStar} alt="" />
-      </div>
-      <div className="template">
-        <h3>How did we do?</h3>
-        <p>
-          Please let us know how we did with your support request. All feedback
-          is appreciated to help us improve our offering!
-        </p>
-      </div>
-      <div className="ratingCard-div">
-        <RatingCard num="1" setRating={setRating} />
-        <RatingCard num="2" setRating={setRating} />
-        <RatingCard num="3" setRating={setRating} />
-        <RatingCard num="4" setRating={setRating} />
-        <RatingCard num="5" setRating={setRating} />
-      </div>
-      <div className="submitBtn-div">
-        <button className="submitBtn">Submit</button>
-      </div>
-    </>
-  );
+    return (
+        <div className={'main-body'}>
+            <div className={'main-card'}>
+                <div className="image-star">
+                    <button className={'starBtn'}>
+                        <img src={iconStar} alt=""/>
+                    </button>
+                </div>
+                <div className="template">
+                    <h3>How did we do?</h3>
+                    <p>
+                        Please let us know how we did with your support request. All feedback
+                        is appreciated to help us improve our offering!
+                    </p>
+                </div>
+                <div className="ratingCard-div">
+                    <RatingCard num="1" setRating={setRating}/>
+                    <RatingCard num="2" setRating={setRating}/>
+                    <RatingCard num="3" setRating={setRating}/>
+                    <RatingCard num="4" setRating={setRating}/>
+                    <RatingCard num="5" setRating={setRating}/>
+                </div>
+                <div className="submitBtn-div">
+                    <button className="submitBtn">Submit</button>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default Rating;
